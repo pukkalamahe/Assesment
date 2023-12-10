85f037b453f17bdd8b7fe6d08645cbc22d40948c
Pukkala Mahendra <153347725+pukkalamahe@users.noreply.github.com>
2023-12-10 15:30:04 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -104,7 +104,9 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
-
+    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
+    for vehicle, rate in rate_coefficients.items():
+        df[vehicle] = df['distance'] * rate
     return df
 
 
