2f381c02c34acdae4a8cb1a21ed0382a16cb6204
Pukkala Mahendra <153347725+pukkalamahe@users.noreply.github.com>
2023-12-10 15:41:52 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -121,5 +121,67 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+    def get_day_name(day):
+        return datetime.strptime(str(day), '%w').strftime('%A')
+
+    weekdays = [0, 1, 2, 3, 4]  # Monday to Friday
+    weekends = [5, 6]  # Saturday and Sunday
+
+    time_ranges = [
+        (time(0, 0, 0), time(10, 0, 0), 0.8),
+        (time(10, 0, 0), time(18, 0, 0), 1.2),
+        (time(18, 0, 0), time(23, 59, 59), 0.8)
+    ]
+
+    start_day_list, start_time_list, end_day_list, end_time_list, moto_list, car_list, rv_list, bus_list, truck_list = [], [], [], [], [], [], [], [], []
+
+    for (id_start, id_end), group in df.groupby(['id_start', 'id_end']):
+        for day in range(7):  # 7 days in a week
+            start_day = get_day_name(day)
+            end_day = get_day_name(day)
+
+            start_date = datetime.combine(datetime.now().date(), time(0, 0, 0)) + timedelta(days=day)
+            end_date = start_date + timedelta(hours=23, minutes=59, seconds=59)
+
+            for start_time, end_time, factor in time_ranges:
+                for curr_date_time in pd.date_range(start=start_date + timedelta(hours=start_time.hour, minutes=start_time.minute, seconds=start_time.second),
+                                                    end=end_date + timedelta(hours=end_time.hour, minutes=end_time.minute, seconds=end_time.second),
+                                                    freq='1H'):
+                    start_day_list.append(start_day)
+                    start_time_list.append(curr_date_time.time())
+                    end_day_list.append(end_day)
+                    end_time_list.append((curr_date_time + timedelta(hours=1)).time())
+
+                    if day in weekdays:
+                        moto_list.append(group['moto'].values[0] * factor)
+                        car_list.append(group['car'].values[0] * factor)
+                        rv_list.append(group['rv'].values[0] * factor)
+                        bus_list.append(group['bus'].values[0] * factor)
+                        truck_list.append(group['truck'].values[0] * factor)
+                    else:
+                        moto_list.append(group['moto'].values[0] * 0.7)
+                        car_list.append(group['car'].values[0] * 0.7)
+                        rv_list.append(group['rv'].values[0] * 0.7)
+                        bus_list.append(group['bus'].values[0] * 0.7)
+                        truck_list.append(group['truck'].values[0] * 0.7)
+
+    df = pd.DataFrame({
+        'start_day': start_day_list,
+        'start_time': start_time_list,
+        'end_day': end_day_list,
+        'end_time': end_time_list,
+        'moto': moto_list,
+        'car': car_list,
+        'rv': rv_list,
+        'bus': bus_list,
+        'truck': truck_list
+    })
 
     return df
+
+df = pd.read_csv("datasets/dataset-3.csv")
+print(calculate_distance_matrix(df))
+print(unroll_distance_matrix(df))
+print(find_ids_within_ten_percentage_threshold(df, reference_id))
+print(calculate_toll_rate(df))
+print(calculate_time_based_toll_rates(df))
